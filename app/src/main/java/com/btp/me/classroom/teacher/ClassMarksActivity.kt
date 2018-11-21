5f775b62283e8f42fc54f512d0e9b9f1522b7322
b16cs006 <meena.13@yahoo.com>
2018-11-22 04:39:24 +0530

    Complete table but not checked

--- app/src/main/java/com/btp/me/classroom/teacher/ClassMarksActivity.kt
+++ app/src/main/java/com/btp/me/classroom/teacher/ClassMarksActivity.kt
@@ -12,11 +12,15 @@ import com.google.firebase.database.ValueEventListener
 
 class ClassMarksActivity : AppCompatActivity() {
 
-    private var n:Int = 0
-    private var m:Int = 0
+    private var n: Int = 0
+    private var m: Int = 0
+
+    private var isTableComplete: Boolean = false
 
     private val userMap = HashMap<String, ArrayList<String>>()
     private val assignmentMap = HashMap<String, ArrayList<String>>()
+
+    private val table = ArrayList<ArrayList<String>>()
 
     private val mRootRef = FirebaseDatabase.getInstance().reference
 
@@ -27,22 +31,22 @@ class ClassMarksActivity : AppCompatActivity() {
         initialize()
     }
 
-    private fun initialize(){}
+    private fun initialize() {}
 
-    private fun getStudentList(){
+    private fun getStudentList() {
 
-        val map = HashMap<String, Map<String,HashMap<String,String>>>()
+        val map = HashMap<String, Map<String, HashMap<String, String>>>()
 
-        mRootRef.child("Classroom/$classId/members").addListenerForSingleValueEvent(object :ValueEventListener{
+        mRootRef.child("Classroom/$classId/members").addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
                 TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                for (data in dataSnapshot.children){
+                for (data in dataSnapshot.children) {
                     if (data.child("as").value == "student") {
-                        val userMap = HashMap<String, HashMap<String,String>>()
-                        val details = HashMap<String,String>()
+                        val userMap = HashMap<String, HashMap<String, String>>()
+                        val details = HashMap<String, String>()
 
                         details["rollNumber"] = data.child("rollNumber").value.toString()
                         details["name"] = data.child("name").value.toString()
@@ -55,29 +59,102 @@ class ClassMarksActivity : AppCompatActivity() {
         })
     }
 
-    private fun getAllUsers(){}        //make a array of userId to index the userId, array of map
+    private fun getAllUsers() {}        //make a array of userId to index the userId, array of map
 
-    private fun getAllAssignment(){
-        mRootRef.child("Assignment/$classId").addListenerForSingleValueEvent(object :ValueEventListener{
+
+    private fun getAllAssignment() {
+        mRootRef.child("Assignment/$classId").addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
                 TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val table = ArrayList<ArrayList<Int>>()
-                var i:Int = 0
-                for (assignmentSnapshot in dataSnapshot.children){
+
+                for ((i, assignmentSnapshot) in dataSnapshot.children.withIndex()) {
                     val assignment = ArrayList<String>()
                     assignment.add(i.toString())
-                    assignment.add(assignmentSnapshot.child(""))
-                }
+                    assignment.add(assignmentSnapshot.child("title").value.toString())
+                    assignment.add(assignmentSnapshot.child("maxMarks").value.toString())
 
+                    assignmentMap[assignmentSnapshot.key.toString()] = assignment
+
+                    mRootRef.child("Classroom/$classId/members").addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onCancelled(p0: DatabaseError) {
+
+                        }
+
+                        override fun onDataChange(dataSnapshot2: DataSnapshot) {
+                            for ((k, member) in dataSnapshot2.children.withIndex()) {
+                                if (member.child("as").value.toString() == "student") {
+                                    val arrayList = ArrayList<String>()
+                                    arrayList.add(k.toString())
+                                    arrayList.add(member.child("name").value.toString())
+                                    arrayList.add(member.child("rollNumber").value.toString())
+                                    userMap[member.key.toString()] = arrayList
+
+                                    val j = userMap[member.key]!![0].toInt()
+
+                                    val marks = assignmentSnapshot.child("marks/${member.key.toString()}/marks").value.toString()
+
+                                    if (marks != "null")
+                                        table[j].add(marks)
+                                    else
+                                        table[j].add(0.toString())
+                                }
+                            }
+
+                            mRootRef.child("Examination/$classId").addListenerForSingleValueEvent(object : ValueEventListener {
+                                override fun onCancelled(p0: DatabaseError) {
+                                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                                }
+
+                                override fun onDataChange(dataSnapshot3: DataSnapshot) {
+
+                                    for ((i, examSnapshot) in dataSnapshot3.children.withIndex()) {
+                                        val exam = ArrayList<String>()
+                                        exam.add(i.toString())
+                                        exam.add(examSnapshot.child("title").value.toString())
+                                        exam.add(examSnapshot.child("maxMarks").value.toString())
+
+                                        assignmentMap[examSnapshot.key.toString()] = exam
+
+                                        mRootRef.child("Classroom/$classId/members").addListenerForSingleValueEvent(object : ValueEventListener {
+                                            override fun onCancelled(dataSnapshot4: DatabaseError) {
+
+                                            }
+
+                                            override fun onDataChange(dataSnapshot4: DataSnapshot) {
+                                                for ((k, member2) in dataSnapshot4.children.withIndex()) {
+                                                    if (member2.child("as").value.toString() == "student") {
+                                                        val arrayList = ArrayList<String>()
+                                                        arrayList.add(k.toString())
+                                                        arrayList.add(member2.child("name").value.toString())
+                                                        arrayList.add(member2.child("rollNumber").value.toString())
+                                                        userMap[member2.key.toString()] = arrayList
+
+                                                        val j = userMap[member2.key]!![0].toInt()
+
+                                                        val marks = examSnapshot.child("marks/${member2.key.toString()}/marks").value.toString()
+
+                                                        if (marks != "null")
+                                                            table[j].add(marks)
+                                                        else
+                                                            table[j].add(0.toString())
+                                                    }
+                                                }
+
+                                                isTableComplete = true
+                                            }
+                                        })
+                                    }
+                                }
+
+                            })
+                        }
+                    })
+                }
             }
 
         })
-    }
-
-    private fun showMarks(){
-
     }
 }
